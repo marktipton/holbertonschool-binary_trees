b05fc192bb2d526248cb6abc9664929a7c120413
marktipton <6675@holbertonstudent.com>
2023-08-18 13:22:54 -0700

    case parent already has elft

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,11 +10,21 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *left_node = NULL;
+	binary_tree_t *temp = NULL;
 
 	if (parent == NULL)
 		return (NULL);
-
-	left_node = binary_tree_node(parent, value);
+	if (parent->left != NULL)
+	{
+		temp = parent->left;
+		left_node = binary_tree_node(parent, value);
+		left_node->left = temp;
+		temp->parent = left_node;
+	}
+	else
+	{
+		left_node = binary_tree_node(parent, value);
+	}
 	if (left_node == NULL)
 		return (NULL);
 	parent->left = left_node;
