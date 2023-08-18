f4be80bc55895c9cc119cd86ff3051686ce3d052
marktipton <6675@holbertonstudent.com>
2023-08-18 13:59:49 -0700

    pre order

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,5 +7,11 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	(void)tree;
+	if (tree == NULL || func == NULL)
+		return;
+	
+	printf("%d\n", tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
