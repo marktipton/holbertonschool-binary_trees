3d6c9d8f1d6af4a9f590e2fb4ad39c180af3f393
marktipton <6675@holbertonstudent.com>
2023-08-18 13:38:32 -0700

    delete node

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,6 +6,12 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	(void)tree;
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
 }
 
