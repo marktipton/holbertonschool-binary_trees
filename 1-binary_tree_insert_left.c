10a942f2f6623814277168302bd35b398fb6da21
marktipton <6675@holbertonstudent.com>
2023-08-18 13:08:17 -0700

    left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,15 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	(void)parent;
-	(void)value;
+	binary_tree_t *left_node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+
+	left_node = binary_tree_node(parent, value);
+	if (left_node == NULL)
+		return (NULL);
+	parent->left = left_node;
+	return (left_node);
 }
 
