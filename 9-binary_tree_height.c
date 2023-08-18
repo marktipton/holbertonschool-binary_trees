5164608cbc9cbfab1fd3849c79b1e5fafbeee6e8
marktipton <6675@holbertonstudent.com>
2023-08-18 14:42:39 -0700

    fixed counting

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,5 +7,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	(void)tree;
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
 }
