6eee4994f9e8f0b355337f717622f8aa1ddab594
marktipton <6675@holbertonstudent.com>
2023-08-21 15:09:27 -0700

    moved initializayion of parent

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,12 +8,11 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent;
 
-	if (node == NULL)
+	if (node == NULL || !(node->parent) || !(node->parent->parent))
 		return (NULL);
+	parent = node->parent;
 
-	if (parent)
-		return (binary_tree_sibling(parent));
-	return (NULL);
+	return (binary_tree_sibling(parent));
 }
