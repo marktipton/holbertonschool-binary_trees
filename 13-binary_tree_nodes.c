1031cbf2ad7b389b61a760915c5d9b79c8bc8e63
marktipton <6675@holbertonstudent.com>
2023-08-21 10:18:48 -0700

    parents

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,4 +1,6 @@
 #include "binary_trees.h"
+#include "11-binary_tree_size.c"
+#include "12-binary_tree_leaves.c"
 /**
  * binary_tree_nodes - counts the parent nodes in binary tree
  *
@@ -7,5 +9,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	(void)tree;
+	size_t size = 0;
+	size_t leaves = 0;
+	size_t parents = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	size = binary_tree_size(tree);
+	leaves = binary_tree_leaves(tree);
+	parents = size - leaves;
+
+	return (parents);
 }
