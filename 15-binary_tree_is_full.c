9dfc42c97b588d0ada9f453c1467a831e972670e
marktipton <6675@holbertonstudent.com>
2023-08-21 12:28:23 -0700

    full = odd number of nodes and balanced

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "11-binary_tree_size.c"
 #include "14-binary_tree_balance.c"
 /**
  * binary_tree_is_full - checks if binary tree is full
@@ -10,9 +11,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if (binary_tree_size(tree) % 2 == 0)
+		return (0);
 	if (binary_tree_balance(tree) == 0)
 		return (1);
+
 	return (0);
-
-
 }
