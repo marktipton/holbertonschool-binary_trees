90216fd73bbd33ac85943ec7ca8264ed343a1c97
marktipton <6675@holbertonstudent.com>
2023-08-21 15:02:23 -0700

    moved intialization of parent

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,10 +7,12 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent;
 
-	if (node == NULL || parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
+
+	parent = node->parent;
 
 	if (parent->left == node)
 	{
