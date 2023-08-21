d76c582bed6383c2c90c05ce42bff68e68e329cf
marktipton <6675@holbertonstudent.com>
2023-08-21 09:25:45 -0700

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,5 +7,16 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	(void)size;
+	int size_left = 0;
+	int size_right = 0;
+	int size;
+
+	if (tree == NULL)
+		return (0);
+	size_left = binary_tree_size(tree->left);
+	size_right = binary_tree_size(tree->right);
+
+	size = 1 + size_left + size_right;
+
+	return (size);
 }
