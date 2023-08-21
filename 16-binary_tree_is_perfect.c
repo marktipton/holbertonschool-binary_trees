a8ebbdb60665597c03a48553ce39857d91c8423b
marktipton <6675@holbertonstudent.com>
2023-08-21 12:41:47 -0700

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,11 +1,21 @@
 #include "binary_trees.h"
+#include "11-binary_tree_size.c"
+#include "14-binary_tree_balance.c"
 /**
  * binary_tree_is_perfect - checks if binary tree is perfect
  *
- * @tree: pointer to root node of tree to count leaves
- * Return: 1 if perfect or 0 if NULL
+ * @tree: pointer to root node of tree
+ * Return: 1 if perfect or 0 if NULL or not perfect
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	(void)tree;
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_size(tree) % 2 == 0)
+		return (0);
+	if (binary_tree_balance(tree) == 0)
+		return (1);
+
+	return (0);
 }
+
